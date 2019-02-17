0aadb1069ded644b582d458e1bcace6c9c00e127
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2019-02-17 10:29:39 +0100

    5: smoother curves

--- 05-lines-displaced-curved/sketch.js
+++ 05-lines-displaced-curved/sketch.js
@@ -7,7 +7,7 @@ const settings = {
 };
 
 const numRows = 40;
-const numCols = 30;
+const numCols = 40;
 
 const margin = 10;
 const sizeInner = size - 2 * margin;
@@ -28,29 +28,40 @@ const sketch = ({ canvas, context, width, height }) => {
     context.translate(margin, margin);
     context.strokeStyle = "black";
     for (let i = 0; i < numRows; i += 1) {
+      const points = [];
+      for (let j = 0; j < numCols; j += 1) {
+        points.push({
+          x: j * widthCell + widthCell / 2,
+          y:
+            heightCell / 2 +
+            (Math.random() < 0.5 ? -1 : 1) *
+              cap((i * j * 4) / (numRows * numCols), 1.4) *
+              Math.floor(Math.random() * heightCell * 0.8)
+        });
+      }
+
       context.save();
       context.translate(0, i * heightCell);
-      context.beginPath();
-      context.moveTo(0, heightCell / 2);
-      let yLast = heightCell / 2;
-      for (let j = 0; j < numCols; j += 1) {
-        const yNext =
-          heightCell / 2 +
-          (Math.random() < 0.5 ? -1 : 1) *
-            // cap to value >1 to allow overlap / crossing of lines
-            cap((i * j * 4) / (numRows * numCols), 1.4) *
-            Math.floor(Math.random() * (heightCell * 0.5));
 
-        const cp = {
-          x: widthCell / 2,
-          y: yLast + (yNext - yLast) * 2
-        };
-        context.quadraticCurveTo(cp.x, cp.y, widthCell, yNext);
-        yLast = yNext;
-        context.translate(widthCell, 0);
+      context.beginPath();
+      context.moveTo(points[0].x, points[0].y);
+      for (let j = 1; j < numCols - 2; j += 1) {
+        /*
+          use points as control points and midpoint as end points to generate smooth curve
+          https://stackoverflow.com/a/7058606
+        */
+        const xEnd = (points[j].x + points[j + 1].x) / 2;
+        const yEnd = (points[j].y + points[j + 1].y) / 2;
+        context.quadraticCurveTo(points[j].x, points[j].y, xEnd, yEnd);
       }
+      context.quadraticCurveTo(
+        points[numCols - 2].x,
+        points[numCols - 2].y,
+        points[numCols - 1].x,
+        points[numCols - 1].y
+      );
+
       context.stroke();
-      context.closePath();
       context.restore();
     }
     context.restore();
